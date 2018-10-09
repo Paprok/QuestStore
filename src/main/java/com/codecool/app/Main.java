02562111d45942b447efa46cb6a6bad78bb41a57
adrianOstromecki <bloodrian@gmail.com>
2018-10-09 14:49:45 +0200

    create view class and enter it as argument to QS controller

--- src/main/java/com/codecool/app/Main.java
+++ src/main/java/com/codecool/app/Main.java
@@ -2,7 +2,8 @@ package com.codecool.app;
 
 public class Main {
     public static void main(String[] args){
-        QuestStore questStore = new QuestStore();
+        QSView qsView = new QSViewConsoleImp();
+        QuestStore questStore = new QuestStore(qsView);
         questStore.run();
     }
 }

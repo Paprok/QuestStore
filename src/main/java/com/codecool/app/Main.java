17cc85a899955e66622fe277722c9ad77ad6b9d4
adrianOstromecki <bloodrian@gmail.com>
2018-10-09 14:24:20 +0200

    add QuestStore class to handle runninng app.

--- src/main/java/com/codecool/app/Main.java
+++ src/main/java/com/codecool/app/Main.java
@@ -1,4 +1,8 @@
 package com.codecool.app;
 
 public class Main {
+    public static void main(String[] args){
+        QuestStore questStore = new QuestStore();
+        questStore.run();
+    }
 }
---
+++ src/main/java/com/codecool/app/QuestStore.java
@@ -0,0 +1,14 @@
+package com.codecool.app;
+
+public class QuestStore {
+    private final String[] menuOptions = {"Login", "Exit"};
+    private QSView view;
+
+    public QuestStore(QSView view){
+        this.view = view;
+    }
+
+    public void run(){
+        
+    }
+}

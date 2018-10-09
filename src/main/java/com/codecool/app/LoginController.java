2fa364aaa9aac7ecfe4429e7bd8d544d4c884d8d
adrianOstromecki <bloodrian@gmail.com>
2018-10-09 15:27:12 +0200

    login controller return account if user is logged

--- src/main/java/com/codecool/app/LoginController.java
+++ src/main/java/com/codecool/app/LoginController.java
@@ -1,11 +1,35 @@
 package com.codecool.app;
 
+import java.util.NoSuchElementException;
+
 public class LoginController {
     private LoginView view;
+    private Account loggedAccount;
+    private boolean isUserLogged;
 
     public LoginController(LoginView view){
         this.view = view;
+        isUserLogged = false;
     }
 
+    public void logIn(){
+        loggedAccount = view.getAccount();
+        isUserLogged = true;
+    }
 
+    public void logOut(){
+        isUserLogged = false;
+    }
+
+    public Account getLoggedAccount(){
+        if (isUserLogged()){
+            return loggedAccount;
+        }
+
+        throw new NoSuchElementException("");
+    }
+
+    public boolean isUserLogged(){
+        return isUserLogged();
+    }
 }
--- src/main/java/com/codecool/app/QuestStore.java
+++ src/main/java/com/codecool/app/QuestStore.java
@@ -1,14 +1,32 @@
 package com.codecool.app;
 
 public class QuestStore {
-    private final String[] menuOptions = {"Login", "Exit"};
+    private final String[] MENU_OPTIONS = {"Login", "Exit"};
     private QSView view;
+    private LoginController loginController;
 
     public QuestStore(QSView view){
         this.view = view;
     }
 
     public void run(){
-        
+        boolean isRunning = true;
+        String choice;
+
+        while (isRunning){
+            view.printMenu(MENU_OPTIONS);
+
+            choice = view.getInput();
+            switch (choice){
+                case "1":
+                    logIn();
+                    break;
+            }
+        }
+    }
+
+    private void logIn(){
+        LoginController loginController = new LoginController(new LoginViewConsoleImp());
+
     }
 }

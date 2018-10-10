48c6f3a086f310923b2ecd6470572fb8fbc67428
Paprok <rafal.becla@gmail.com>
2018-10-10 11:15:06 +0200

    added queries to create database and account tables

---
+++ queries/codecoolers.sql
@@ -0,0 +1,19 @@
+﻿-- Table: public.codecoolers
+
+DROP TABLE public.codecoolers;
+
+CREATE TABLE public.codecoolers
+(
+  nick text NOT NULL,
+  balance integer,
+  level integer,
+  earned integer,
+  CONSTRAINT codecoolers_nick_fkey FOREIGN KEY (nick)
+      REFERENCES public.accounts (nick) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.codecoolers
+  OWNER TO queststore;

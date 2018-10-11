4cd90a1666d78345e9db666dc58c07b05e1a6fe2
adrianOstromecki <bloodrian@gmail.com>
2018-10-11 17:01:30 +0200

    implement most of admin options

--- src/main/java/com/codecool/app/user/controller/AdminController.java
+++ src/main/java/com/codecool/app/user/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.codecool.app.user.controller;
 import com.codecool.app.dao.AppDAOs;
 import com.codecool.app.login.Account;
 import com.codecool.app.messages.ErrorMessages;
+import com.codecool.app.messages.UserCreationMessages;
 import com.codecool.app.user.model.Mentor;
 import com.codecool.app.user.view.AdminView;
 
@@ -14,6 +15,7 @@ public class AdminController implements UserController{
             "Edit Mentor", "Create class", "Create level of experience", "Exit"
     };
     private ErrorMessages errorMessages;
+    private UserCreationMessages userCreationMessages;
     private AdminView view;
     private AppDAOs appDAOs;
     private boolean isRunning;
@@ -23,6 +25,7 @@ public class AdminController implements UserController{
         this.appDAOs = appDAOs;
         isRunning = true;
         errorMessages = new ErrorMessages();
+        userCreationMessages = new UserCreationMessages();
     }
 
     @Override
@@ -42,13 +45,28 @@ public class AdminController implements UserController{
             view.printError(errorMessages.getEMPTY_INPUT_MESSAGE());
             return;
         }
+        int id;
 
         switch (userChoice){
             case "1":
                 handleCreatingAccount();
                 break;
             case "2":
-                
+                view.showMentorsAndTheirClassess(appDAOs.getDAOMentors().getMentors());
+                break;
+            case "3":
+                id = chooseId();
+                view.showMentor(appDAOs.getDAOMentors().getMentor(id));
+                break;
+            case "4":
+                id = chooseId();
+                Mentor mentor = handleCreatingMentor(id);
+                appDAOs.getDAOMentors().updateMentor(id, mentor);
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
             case "7":
                 isRunning = false;
                 break;
@@ -60,6 +78,11 @@ public class AdminController implements UserController{
 
     }
 
+    private int chooseId(){
+        view.println(userCreationMessages.getASK_FOR_ID());
+        return Integer.parseInt(view.getInput());
+    }
+
     private void handleCreatingAccount(){
         Account account = view.getAccountInformation();
         appDAOs.getDAOAccounts().insertAccount(account);
@@ -68,7 +91,8 @@ public class AdminController implements UserController{
         
         switch (account.getAccessLevel()){
             case MENTOR:
-                handleCreatingMentor(user_id);
+                Mentor newMentor = handleCreatingMentor(user_id);
+                appDAOs.getDAOMentors().insertMentor(newMentor);
                 break;
             case CODECOOLER:
                 // TO DO
@@ -76,15 +100,16 @@ public class AdminController implements UserController{
         }
     }
 
-    private void handleCreatingMentor(int user_id){
+    private Mentor handleCreatingMentor(int user_id){
         boolean areMentorInformationsValid = false;
+        Mentor mentor = null;
 
-        while (! areMentorInformationsValid){
-            Mentor mentor = view.getMentorInformation();
-            // Save Mentor to DB
-            // If Mentor can be saved successfully, exit loop
-            // Delete below statement:
+        do{
+            mentor = view.getMentorInformation();
+            // Check if informations are valid
             areMentorInformationsValid = true;
-        }
+        } while (! areMentorInformationsValid);
+
+        return mentor;
     }
 }

fb7d29df816008ddc120c31adf42a0d5c72fd2ce
Paprok <rafal.becla@gmail.com>
2018-10-12 11:29:22 +0200

    added teams table to DB

---
+++ queries/teams.sql
@@ -0,0 +1,14 @@
+﻿-- Table: public.teams
+
+-- DROP TABLE public.teams;
+
+CREATE TABLE public.teams
+(
+  team_id SERIAL NOT NULL PRIMARY KEY,
+  name text    
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.teams
+  OWNER TO queststore;

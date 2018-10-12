3f93e0274aa964bf577e8899cb6de239d4cf998e
adrianOstromecki <bloodrian@gmail.com>
2018-10-12 11:59:36 +0200

    finish codecoolerDAO SQL implementation

--- src/main/java/com/codecool/app/dao/SQLImpl/DAOCodecoolersSQL.java
+++ src/main/java/com/codecool/app/dao/SQLImpl/DAOCodecoolersSQL.java
@@ -23,14 +23,15 @@ public class DAOCodecoolersSQL implements DAOCodecoolers {
     @Override
     public void insertCodecooler (Codecooler newCodecooler) {
         try {
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO codecoolers (user_id, name, surname, nick, balance, earned) " +
-                    "VALUES (?, ?, ?, ?, ?, ?)");
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO codecoolers (user_id, name, surname, nick, balance, total_exp, class_id) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?)");
             ps.setInt(1, newCodecooler.getId());
             ps.setString(2, newCodecooler.getFirstName());
             ps.setString(3, newCodecooler.getLastName());
             ps.setString(4, newCodecooler.getNickname());
             ps.setInt(5, newCodecooler.getBalance());
-            ps.setInt(6, newCodecooler.getEarned());
+            ps.setInt(6, newCodecooler.getTotalExp());
+            ps.setInt(7, newCodecooler.getClassId());
             ps.execute();
             ps.close();
         } catch (SQLException e){
@@ -42,14 +43,15 @@ public class DAOCodecoolersSQL implements DAOCodecoolers {
     @Override
     public void updateCodecooler(int id, Codecooler updatedCodecooler) {
         try {
-            PreparedStatement ps = connection.prepareStatement(" UPDATE codecoolers SET name=?, surname=?, nick=?, balance=?, earned=?" +
+            PreparedStatement ps = connection.prepareStatement(" UPDATE codecoolers SET name=?, surname=?, nick=?, balance=?, earned=?, class_id=?" +
                             "WHERE user_id =?");
-            ps.setString(1, updatedCodecooler.getFirstName();
+            ps.setString(1, updatedCodecooler.getFirstName());
             ps.setString(2, updatedCodecooler.getLastName());
             ps.setString(3, updatedCodecooler.getNickname());
             ps.setInt(4, updatedCodecooler.getBalance());
-            ps.setInt(5, updatedCodecooler.getEarned());
-            ps.setInt(6, id);
+            ps.setInt(5, updatedCodecooler.getTotalExp());
+            ps.setInt(6, updatedCodecooler.getClassId());
+            ps.setInt(7, id);
             ps.executeUpdate();
             ps.close();
         } catch (SQLException e){
@@ -78,17 +80,10 @@ public class DAOCodecoolersSQL implements DAOCodecoolers {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setInt(1, id);
             ResultSet resultSet = ps.executeQuery();
-            if (resultSet.next()) {
-                String name = resultSet.getString("name");
-                String surname = resultSet.getString("surname");
-                String nickname = resultSet.getString("nick");
-                int balance = resultSet.getInt("balance");
-                int totalExp = resultSet.getInt("total_exp");
-                int userId = resultSet.getInt("user_id");
-                int classId = resultSet.getInt("class_id");
-                codecooler = new Codecooler(name, surname);
-                // TO DO: Change codecooler constructor and add set methods
+            if (resultSet.next()){
+                return extractCodecoolerFromResultSet(resultSet);
             }
+
         }catch (SQLException e) {
             System.out.println("Couldn't find codecooler with given id");
         }
@@ -103,19 +98,28 @@ public class DAOCodecoolersSQL implements DAOCodecoolers {
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet resultSet = ps.executeQuery();
             while (resultSet.next()) {
-                int id = resultSet.getInt("user_id");
-                String name = resultSet.getString("name");
-                String surname = resultSet.getString("surname");
-                String nickname = resultSet.getString("nick");
-                Codecooler codecooler =new Codecooler(name, surname);
-                // Change codecooler constructor and add set methods
-                codecooler.setId(id);
-                codecoolers.add(codecooler);
-
+                codecoolers.add(extractCodecoolerFromResultSet(resultSet));
             }
         }catch (SQLException e) {
-            System.out.println("Couldn't find mentor with given id");
+            System.out.println("Couldn't find codecooler with given id");
         }
-        return mentors;
+        return codecoolers;
+    }
+
+    private Codecooler extractCodecoolerFromResultSet(ResultSet resultSet) throws SQLException{
+            String name = resultSet.getString("name");
+            String surname = resultSet.getString("surname");
+            String nickname = resultSet.getString("nick");
+            int balance = resultSet.getInt("balance");
+            int totalExp = resultSet.getInt("total_exp");
+            int userId = resultSet.getInt("user_id");
+            int classId = resultSet.getInt("class_id");
+            Codecooler codecooler = new Codecooler(name, surname);
+            codecooler.setNickname(nickname);
+            codecooler.setBalance(balance);
+            codecooler.setTotalExp(totalExp);
+            codecooler.setId(userId);
+            codecooler.setClassId(classId);
+            return codecooler;
     }
 }
--- src/main/java/com/codecool/app/user/model/Codecooler.java
+++ src/main/java/com/codecool/app/user/model/Codecooler.java
@@ -1,12 +1,19 @@
 package com.codecool.app.user.model;
 
 public class Codecooler {
+    private int id;
     private String firstName;
     private String lastName;
+    private String nickname;
+    private int balance;
+    private int totalExp;
+    private int classId;
 
     public Codecooler(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        balance = 0;
+        totalExp = 0;
     }
 
     public String getFirstName() {
@@ -23,5 +30,45 @@ public class Codecooler {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getTotalExp() {
+        return totalExp;
+    }
+
+    public void setTotalExp(int totalExp) {
+        this.totalExp = totalExp;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
     }
 }

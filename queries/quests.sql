8bcca38ca80d89939fca930c69ecb754cb67aa83
Paprok <rafal.becla@gmail.com>
2018-10-12 11:28:25 +0200

    added quests table to DB

---
+++ queries/quests.sql
@@ -0,0 +1,18 @@
+﻿-- Table: public.quests
+
+-- DROP TABLE public.quests;
+
+CREATE TABLE public.quests
+(
+  quest_id SERIAL PRIMARY KEY,
+  q_name TEXT NOT NULL UNIQUE,
+  description TEXT,
+  coin_reward TEXT,
+  type TEXT,
+  quest_type TEXT
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.quests
+  OWNER TO queststore;

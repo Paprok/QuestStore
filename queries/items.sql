d4fd69f004d66324c7787dffc5f1b1cc7ec360f6
Paprok <rafal.becla@gmail.com>
2018-10-12 11:28:06 +0200

    added items table to DB

---
+++ queries/items.sql
@@ -0,0 +1,17 @@
+﻿-- Table: public.items
+
+-- DROP TABLE public.items;
+
+CREATE TABLE public.items
+(
+  item_id SERIAL PRIMARY KEY,
+  i_name TEXT NOT NULL UNIQUE,
+  description TEXT,
+  price INTEGER,
+  type TEXT
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.items
+  OWNER TO queststore;

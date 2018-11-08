eef42b88edb2cbd65bced30dcb2878a94100c9e0
Paprok <rafal.becla@gmail.com>
2018-11-08 17:40:55 +0100

    edited Class model to include name

--- src/main/java/com/codecool/app/user/model/Class.java
+++ src/main/java/com/codecool/app/user/model/Class.java
@@ -5,17 +5,36 @@ import java.util.List;
 public class Class {
     private List<Mentor> mentors;
     private List<Codecooler> codecoolers;
+    private String name;
+
+    public Class() {}
 
     public Class(List<Mentor> mentors, List<Codecooler> codecoolers) {
         this.mentors = mentors;
         this.codecoolers = codecoolers;
     }
 
+    public void setMentors(List<Mentor> mentors) {
+        this.mentors = mentors;
+    }
+
     public List<Mentor> getMentors() {
         return mentors;
     }
 
+    public void setCodecoolers(List<Codecooler> codecoolers) {
+        this.codecoolers = codecoolers;
+    }
+
     public List<Codecooler> getCodecoolers() {
         return codecoolers;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }

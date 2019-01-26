5b2c0f9a89f85b2d7c3a5d6770ac9a6551f002ed
Paprok <rafal.becla@gmail.com>
2019-01-26 14:56:02 +0100

    cleancode

--- src/main/java/com/codecool/app/connectors/SQLConnector.java
+++ src/main/java/com/codecool/app/connectors/SQLConnector.java
@@ -5,6 +5,16 @@ import java.sql.*;
 public class SQLConnector {
     private static Connection connection = null;
 
+    public static Connection getConnection(){
+        if(connection == null){
+            try {
+                createConnection();
+            } catch (SQLException e) {
+                System.out.println("Couldn't create connection to database");
+            }
+        } return connection;
+    }
+
     private static void createConnection() throws SQLException {
         try {
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/queststore", "queststore", "admin123");
@@ -15,15 +25,5 @@ public class SQLConnector {
             System.exit(0);
         }
         System.out.println("Opened database successfully");
-    }
-
-    public static Connection getConnection(){
-        if(connection == null){
-            try {
-                createConnection();
-            } catch (SQLException e) {
-                System.out.println("Couldn't create connection to database");
-            }
-        } return connection;
     }
 }
